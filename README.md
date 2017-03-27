68890d33a732c82a20be3b9d895ce4a805d94e37
John Vincent Uy Calacsan <johnvuc@hotmail.com>
2017-03-27 17:50:25 +0200

    Test-work
    Work of VIVID CIPHERS

---
+++ README.md
@@ -0,0 +1,18 @@
+# Vivid Ciphers Website
+
+You need to follow some rules to avoid chaos in code.  
+Before you start, please take a look at:
+* https://github.com/airbnb/css
+* https://github.com/airbnb/javascript
+
+We need to work as a team, as one fullstack developer.  
+
+Cheers!
+
+
+## Test pages  
+
+You can test it here:  
+* http://nikitasherbakov.com/v/  
+* http://nikitasherbakov.com/v/reg.html  
+* http://nikitasherbakov.com/v/login.html  
---
+++ js/header-queries.js
@@ -0,0 +1,46 @@
+; (function () {
+
+    'use strict'
+    let wrap = document.querySelector('.wrap-max1140px');
+    let header = document.querySelector('header');
+    let links = document.querySelectorAll('.main > a');
+    let mq = window.matchMedia("(max-width: 600px)");
+    let hamburgerNav = document.querySelector('.hamburger');
+    mq.addListener(alignLeft);
+
+    for (let i=0; i<links.length; i++) {
+      let clonedNode = links[i].cloneNode(true);
+      clonedNode.classList.add('block');
+      hamburgerNav.appendChild(clonedNode);
+    }
+
+    let hamburgerIcon = document.createElement('div');
+    let classArray = ['hamburger-button','col-xs-2','disabled'];
+    classArray.forEach(function(el){
+      hamburgerIcon.classList.add(el);
+    });
+    header.appendChild(hamburgerIcon);
+
+    hamburgerIcon.addEventListener('click', function(){
+      hamburgerNav.classList.toggle('transition-left');
+    });
+
+    (window.innerWidth <= 600) ? alignLeft() : false;
+
+    window.addEventListener('resize', function(){
+      (window.innerWidth > 600 && hamburgerNav.classList.contains('transition-left')) ? hamburgerNav.classList.toggle('transition-left') : false;
+      let contentSize = Math.max( document.body.scrollHeight, document.body.offsetHeight,
+                         document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+      hamburgerNav.style.height = contentSize + 'px';
+    });
+
+    function alignLeft(){
+      let logo = document.querySelector('.logo');
+      logo.classList.toggle('center-xs');
+      logo.classList.toggle('col-xs-12');
+      logo.classList.toggle('col-xs-10');
+      hamburgerIcon.classList.toggle('disabled');
+    };
+
+
+ }());
